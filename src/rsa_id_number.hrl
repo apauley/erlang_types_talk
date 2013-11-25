a1bf1fa63c34858ddf16d773803aa853208dd65b
Andreas Pauley <apauley+github@gmail.com>
2013-11-25 09:49:51 +0200

    Add rsa_id_number.hrl from erlcdt

---
+++ src/rsa_id_number.hrl
@@ -0,0 +1,15 @@
+%% http://geekswithblogs.net/willemf/archive/2005/10/30/58561.aspx
+%% Format:
+%% {YYMMDD}{G}{SSS}{C}{A}{Z}
+%% YYMMDD : Date of birth.
+%% G  : Gender. 0-4 Female; 5-9 Male.
+%% SSS  : Sequence No. for DOB/G combination.
+%% C  : Citizenship. 0 SA; 1 Other.
+%% A  : Usually 8, or 9 [can be other values]
+%% Z  : Checksum digit
+-record(rsa_id_number, {birth_date     :: list(calendar:date()),
+                        gender_digit   :: 0..9,
+                        sequence_nr    :: 0..999,
+                        citizen_digit  :: 0 | 1,
+                        digit_a        :: 0..9,
+                        checksum_digit :: 0..9}).
